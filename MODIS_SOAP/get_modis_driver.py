3e78cf7b69dfd560e9815749c5593dba20c9a7be
Josh Laughner <joshlaugh5@gmail.com>
2018-05-22 12:30:09 -0700

    get_modis_driver modified to accept command line args and try to redownload missing files

--- MODIS_SOAP/get_modis_driver.py
+++ MODIS_SOAP/get_modis_driver.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 from . import automodis as am
 
+import argparse
 import datetime as dt
 from glob import glob
 import os
@@ -12,14 +13,19 @@ import sys
 import pdb
 
 modis_date_re = re.compile('(?<=A)\d{7}')
+default_min_start_date = dt.datetime.today() - dt.timedelta(days=-90)
 USERAGENT = 'tis/download.py_1.0--' + sys.version.replace('\n','').replace('\r','')
 
-def get_product_last_date(product, path):
+def get_product_last_date(product, path, min_start_date=None):
     """
     Searches the directory given by path to find yearly subdirectories and finds
     the most recent file for product, assuming that the files start with the
     product name
     """
+    if min_start_date is None:
+        min_start_date = default_min_start_date
+
+
     year_subdirs = sorted(glob(os.path.join(path, '20*')))
     last_year = year_subdirs[-1]
     product_files = sorted(glob(os.path.join(last_year, '{}*'.format(product))))
@@ -27,8 +33,9 @@ def get_product_last_date(product, path):
     most_recent_datestr = modis_date_re.search(most_recent_file).group()
     return dt.datetime.strptime(most_recent_datestr, '%Y%j')
 
-def list_product_urls(product, collection, path):
-    first_download_date = get_product_last_date(product, path) + dt.timedelta(days=1)
+
+def list_product_urls(product, collection, path, min_start_date=None):
+    first_download_date = get_product_last_date(product, path, min_start_date=min_start_date) + dt.timedelta(days=1)
     file_urls = am.get_modis(products=product, collection=collection, startTime=first_download_date.strftime('%Y-%m-%d %H:%M:%S'),
                              endTime=dt.datetime.today().strftime('%Y-%m-%d %H:%M:%S'), dayNightBoth='DB')
     if file_urls is None:
@@ -104,9 +111,10 @@ def geturl(url, token=None, out=None):
             print('curl GET error message: %' + (e.message if hasattr(e, 'message') else e.output), file=sys.stderr)
         return None
 
-def download_product(product, collection, path, verbose=0):
+
+def download_product(product, collection, path, min_start_date=None, verbose=0):
     token = get_earthdata_token()
-    urls = list_product_urls(product, collection, path)
+    urls = list_product_urls(product, collection, path, min_start_date=min_start_date)
     for link in urls:
         file_datestr = modis_date_re.search(link).group()
         file_date = dt.datetime.strptime(file_datestr, '%Y%j')
@@ -116,12 +124,19 @@ def download_product(product, collection, path, verbose=0):
             os.mkdir(year_directory)
 
         file_basename = os.path.basename(link)
-        with open(os.path.join(year_directory, file_basename), 'wb') as save_obj:
+        file_fullname = os.path.join(year_directory, file_basename)
+        if os.path.isfile(file_fullname) and os.path.getsize(file_fullname) > 0:
+            if verbose > 1:
+                print('File {} already exists and size is > 0; not re-downloaded'.format(file_fullname))
+            continue
+
+        with open(file_fullname, 'wb') as save_obj:
             if verbose > 0:
                 print('Downloading {} to {}'.format(link, os.path.join(year_directory, file_basename)))
             geturl(link, token, save_obj)
 
-def main(verbose=0):
+
+def main(min_start_date=None, verbose=0):
     modis_path = os.getenv('MODDIR')
     modis_alb_dir = os.path.join(modis_path, 'MCD43D')
     modis_cloud_dir = os.path.join(modis_path, 'MYD06_L2')
@@ -135,7 +150,27 @@ def main(verbose=0):
     for product, collection, path in products:
         if verbose > 0:
             print('Now downloading the {} (collection {}) product'.format(product, collection))
-        download_product(product, collection, path, verbose=verbose)
+        download_product(product, collection, path, min_start_date=min_start_date, verbose=verbose)
+
+
+def parse_cl_date(datestr):
+    if datestr is None:
+        return default_min_start_date
+    else:
+        return dt.datetime.strptime(datestr, '%Y-%m-%d')
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Driver to download MODIS albedo and cloud data')
+    parser.add_argument('-d', '--min-start-date', default=None, type=parse_cl_date,
+                        help='The required first date to check if MODIS files exist. This program will start from this '
+                             'date or the most recent MODIS file, if the most recent file is before this date. Default '
+                             'is 90 days ago.')
+    parser.add_argument('-v', '--verbose', action='count', help='Increase verbosity to the terminal')
+
+    return vars(parser.parse_args())
+
 
 if __name__ == '__main__':
-    main(verbose=1)
+    args_dict = parse_args()
+    main(**args_dict)
